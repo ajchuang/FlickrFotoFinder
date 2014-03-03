cea38131646610096dc16a7647a9d10cc921a988
Alfred Huang <alfredhuang@Alfredteki-MacBook-Air.local>
2014-03-03 09:22:35 -0500

    Halfway

--- src/com/example/picfinder/MainActivity.java
+++ src/com/example/picfinder/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.picfinder;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.view.*;
 import android.view.View.*;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import android.util.*;
 import android.content.Intent;
+import android.database.Cursor;
 import android.provider.*;
 
 import android.text.Editable;
 import android.os.AsyncTask;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.*;
 
 import org.apache.http.client.HttpClient;
@@ -21,6 +29,7 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.xmlpull.v1.XmlPullParser;
 
 // Sample code: show image from internet in the image viewer
 // Note: Need user permission -> INTERNET
@@ -29,11 +38,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 // } catch (Exception e) {
 // }
 
-// Hint: For example, 
-// http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={api key 
-// here}&tags=Emma&extras=date_taken,owner_name,description
-
 public class MainActivity extends Activity implements OnClickListener {
+	
+	final static int 	M_REQCODE_CONTACT = 1;
+	final static String M_LOG_TAG = "Mainactivity";
 	
 	final static String m_searchUrl_1 = 
 			"http://api.flickr.com/services/rest/?method=flickr.photos.search&" + 
@@ -42,42 +50,80 @@ public class MainActivity extends Activity implements OnClickListener {
 	final static String m_searchUrl_2 = 	
 			"&extras=date_taken,owner_name,description";
 	
+	static ProgressDialog	m_progDialog;
+	
 	public static String flickrUrlBuilder (String tag) {
 		return new String (m_searchUrl_1 + tag + m_searchUrl_2);
 	}
 	
 	// @lfred inner working class
-	private static class WorkThread extends AsyncTask<String, Void, String> {
+	private static class WorkThread extends AsyncTask<String, Void, Integer> {
 		
-		@SuppressWarnings("unused")
-		protected void onPostExecute (String... results) {
+		@Override
+		protected void onPostExecute (Integer result) {
+			
+				
+			Log.i (M_LOG_TAG, "WorkThread: onPostExecute");
+			MainActivity.m_progDialog.dismiss ();
+			
+			if (result.intValue() != 0)
+				Log.i (M_LOG_TAG, "onPostExecute - error");
 			return;
 		}
 
 		@Override
-		protected String doInBackground (String... params) {
+		protected Integer doInBackground (String... params) {
 			
 			if (params.length == 0) 
-				return null;
+				return -1;
 			
 			String url = makeUrl (params);
-			HttpClient client = new DefaultHttpClient (); 
+			int returnCode = 0;
 			
+			// @lfred: new
 			try {
-				HttpGet get = new HttpGet (url);
-				ResponseHandler<String> responseHandler = new BasicResponseHandler();
-                String setServerString = client.execute (get, responseHandler);
-                Log.i ("Mainactivity", "Server rsp: " + setServerString);
-      
-                // Show response on activity 
+				
+				// Step 1. get the XML file using REST
+				String tempPath = null;
+				File outFile = File.createTempFile ("result", ".xml");
+				FileOutputStream f = new FileOutputStream (outFile);
+				
+				URL u = new URL (url);
+			    HttpURLConnection c = (HttpURLConnection) u.openConnection ();
+			    c.setRequestMethod ("GET");
+			    c.setDoOutput (true);
+			    c.connect ();
+			    
+			    InputStream in = c.getInputStream ();
 
-                //content.setText(SetServerString);
+			    byte[] buffer = new byte[1024];
+			    int len = 0;
+			    int total_len = 0;
+			    
+			    while ((len = in.read (buffer)) != -1) {
+			         f.write (buffer, 0, len);
+			         total_len += len;
+			    }
+			    
+			    Log.i (M_LOG_TAG, "Total length = " + Integer.toString (total_len));
+
+			    f.close ();
+			    tempPath = outFile.getAbsolutePath ();
+			    in.close ();
+			    
+			    // Step 2: parsing the XML file
+			    // Step 2.1: read <photos ... >
+			    // Step 2.2: read <photo id>, <farm id>, <server_id>, <secret>
+			    
+				
 			} catch (Exception e) {
-				e.printStackTrace ();
+				Log.i (M_LOG_TAG, "Ooops, IO exception");
+				returnCode = -1;
+				
 			}
 			
-			Log.i ("Mainactivity", "I am a worker:" + params[0]);
-			return null;
+			Log.i (M_LOG_TAG, "I am a worker:" + params[0]);
+			return returnCode;
 		}
 		
 		String makeUrl (String... params) {
@@ -89,15 +135,13 @@ public class MainActivity extends Activity implements OnClickListener {
 			
 			String url = m_searchUrl_1 + searchKeyWords + m_searchUrl_2;
 			
-			Log.i ("Mainactivity", "the query URL: " + url);
+			Log.i (M_LOG_TAG, "the query URL: " + url);
 			return url;
-		}
-		
-		
+		}		
 	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
@@ -111,40 +155,45 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater ().inflate (R.menu.main, menu);
 		return true;
 	}
 	
 	@Override
 	public void onClick (View v) {
 		
-		int id = v.getId();
+		int id = v.getId ();
 		
 		switch (id) {
 		
 			case R.id.searchBtn: {
-				Log.i ("Mainactivity", "search button clicked");
+				
+				Log.i (M_LOG_TAG, "search button clicked");
 				
 				EditText text = (EditText) findViewById (R.id.keywordText);
-				String ed = text.getText ().toString ();
+				String ketword = text.getText ().toString ();
 				
-				Log.i ("Mainactivity", ed);
+				Log.i (M_LOG_TAG, ketword);
 				
-				new WorkThread ().execute (ed);
-				
+				if (ketword.length () == 0) {
+					Toast t  = Toast.makeText (MainActivity.this, "Empty Keyword", Toast.LENGTH_LONG);
+					t.show ();
+				} else {
+					startSearchTask (ketword);
+				}
 			} 
 			break;
 			
 			case R.id.contactBtn: {
-				Log.i ("Mainactivity", "contact button clicked");
+				Log.i (M_LOG_TAG, "contact button clicked");
 				
 				// @lfred: create an intent to open the contact windows
 				Intent it = new Intent (Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-				startActivityForResult (it, 1);
+				startActivityForResult (it, M_REQCODE_CONTACT);
 			} break;
 			
 			default:
-				Log.i ("Mainactivity", "unknown button clicked");
+				Log.i (M_LOG_TAG, "unknown button is pressed");
 			break;
 		
 		}
@@ -152,7 +201,63 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	@Override
 	public void onActivityResult (int reqCode, int resultCode, Intent data) {
-		super.onActivityResult(reqCode, resultCode, data);
-		Log.i ("Mainactivity", "return from activity");
+		
+		super.onActivityResult (reqCode, resultCode, data);
+		
+		if (reqCode == M_REQCODE_CONTACT) {
+		
+			if (resultCode != RESULT_OK) {
+			
+				Toast t = Toast.makeText (MainActivity.this, "No contact selected", Toast.LENGTH_LONG);
+				t.show ();
+				return;
+			
+			} else {
+				
+				if (data != null) {
+		            Uri u = data.getData ();
+		            Log.i (M_LOG_TAG, "URI: " + u.toString());
+		            
+		            int idx;
+		            String id, name, phone, hasPhone;
+		            
+		            Cursor cursor = getContentResolver().query (u, null, null, null, null);
+		            
+		            if (cursor.moveToFirst()) {
+		                idx = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+		                id = cursor.getString(idx);
+
+		                idx = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+		                name = cursor.getString(idx);
+
+		                idx = cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+		                hasPhone = cursor.getString(idx);
+		                
+		                Log.i (M_LOG_TAG, "Contact Name: " + name);
+		                startSearchTask (name);
+		            }
+		            
+				} else {
+					Toast t  = Toast.makeText (MainActivity.this, "Empty data", Toast.LENGTH_LONG);
+					t.show ();
+					return;
+				}
+			
+				// get the contact info and use the async task to run the task
+			}
+		} else {
+			Log.i (M_LOG_TAG, "Unknown activity");
+		}
+		
+		Log.i (M_LOG_TAG, "return from activity");
+	}
+	
+	void startSearchTask (String key) {
+		m_progDialog = 
+			ProgressDialog.show (MainActivity.this, "Please wait ...", "Searching Data ...", true);					
+		m_progDialog.setCancelable (false);
+			
+		// start the worker thread
+		new WorkThread ().execute (key);
 	}
 }
